c28ae2ae94eda544519903a3eec873e36da39ba6
Erghian Ranggata Apryanto <161951115+HzardGenmu@users.noreply.github.com>
2025-09-24 10:05:02 +0700

    initial commit

---
+++ ojs_config/config.inc.php
@@ -0,0 +1,13 @@
+[general]
+installed = On
+base_url = "http://ojs.localhost"
+
+[database]
+driver = mysqli
+host = ojs_db
+username = ojs_user
+password = ojs_pass
+name = ojs_db
+
+[email]
+mailer = mail
