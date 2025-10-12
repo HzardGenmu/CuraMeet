bb7716ccd526cb91b997932afeffba10087f86b6
rakafdil <rakafadillah123@gmail.com>
2025-10-13 05:11:30 +0700

    change to vite and edit cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,11 +1,19 @@
 <?php
 
 return [
-    'paths' => [], // Disable Laravel CORS
-    'allowed_methods' => [],
-    'allowed_origins' => [],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // This might look different for you
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:3000',
+        'https://curameet.duckdns.org/'
+    ],
+
     'allowed_origins_patterns' => [],
-    'allowed_headers' => [],
+
+    'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
     'supports_credentials' => false,
