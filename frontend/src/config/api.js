ee1ea844eb9a23f8d37d2398ad13c1e4db93845b
rakafdil <rakafadillah123@gmail.com>
2025-10-13 02:10:38 +0700

    cors edit

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "https://api.curameet.duckdns.org/api";
+// Ganti dari HTTPS ke HTTP untuk development
+const API_BASE_URL = "http://api.curameet.duckdns.org/api";
 
 export const apiConfig = {
   baseURL: API_BASE_URL,
