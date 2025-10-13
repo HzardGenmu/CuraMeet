c5640ace821bad35eea52633be64d548bc166647
rakafdil <rakafadillah123@gmail.com>
2025-10-13 22:27:04 +0700

    update reg and cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,18 +1,9 @@
 <?php
 
 return [
-
-    'paths' => ['*'],
-
+    'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'https://curameet.duckdns.org/'
-    ],
-
-    'allowed_origins_patterns' => [],
-
+    'allowed_origins' => ['*'], // atau ['*'] untuk semua
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
